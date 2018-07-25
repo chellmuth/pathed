85ec9a8a3b1726f489746e2c2418a5992de9ce5f
Chris Hellmuth <chris@usenotion.com>
2018-07-25 07:48:34 -0700

    Variable name

--- src/material.cpp
+++ src/material.cpp
@@ -11,7 +11,7 @@ Color shade(const Intersection &intersection, const Scene &scene)
     Point3 light = scene.light();
 
     Vector3 lightDirection = (light - intersection.point).toVector().normalized();
-    float LDotNormal = lightDirection.dot(intersection.normal);
+    float lightDotNormal = lightDirection.dot(intersection.normal);
 
-    return intersection.color * fmaxf(0.f, LDotNormal);
+    return intersection.color * fmaxf(0.f, lightDotNormal);
 }
