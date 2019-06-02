71beafad2b3c97febfe8fd6e1c61067d961ee662
Chris Hellmuth <chellmuth@gmail.com>
2019-06-02 10:05:50 -0700

    Use bvh for intersection

--- src/scene.cpp
+++ src/scene.cpp
@@ -30,21 +30,5 @@ std::shared_ptr<Camera> Scene::getCamera()
 
 Intersection Scene::testIntersect(const Ray &ray) const
 {
-    Intersection result = {
-        .hit = false,
-        .t = std::numeric_limits<float>::max(),
-        .point = Point3(0.f, 0.f, 0.f),
-        .wi = Vector3(0.f),
-        .normal = Vector3(0.f),
-        .material = nullptr
-    };
-
-    for (std::shared_ptr<Surface> surfacePtr : m_surfaces) {
-        Intersection intersection = surfacePtr->testIntersect(ray);
-        if (intersection.hit && intersection.t < result.t) {
-            result = intersection;
-        }
-    }
-
-    return result;
+    return m_bvh->testIntersect(ray);
 }
