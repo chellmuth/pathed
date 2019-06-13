c68882ca67bc0a569f97896197d5be468d106a82
Chris Hellmuth <chellmuth@gmail.com>
2019-06-13 16:11:00 -0700

    Get rid of path length 1s

--- app/main.cpp
+++ app/main.cpp
@@ -57,10 +57,10 @@ void samplePixel(
 
     Color color = integrator.L(intersection, scene, random, bounceCount, sample);
 
-    Color emit = intersection.material->emit();
-    if (!emit.isBlack()) {
-        color = emit;
-    }
+    // Color emit = intersection.material->emit();
+    // if (!emit.isBlack()) {
+    //     color = emit;
+    // }
 
     radianceLookup[3 * (row * width + col) + 0] += color.r();
     radianceLookup[3 * (row * width + col) + 1] += color.g();
