c20740abd4fed9c971d8d330aa7b3d47d065509a
Chris Hellmuth <chellmuth@gmail.com>
2019-06-18 17:47:04 -0700

    Cruft

--- tools/irradiance_field.py
+++ tools/irradiance_field.py
@@ -71,20 +71,6 @@ def run(sample):
 
     plt.show()
 
-# # unit area ellipse
-# rx, ry = 3., 1.
-# area = rx * ry * np.pi
-# theta = np.arange(0, 2 * np.pi + 0.01, 0.1)
-# verts = np.column_stack([rx / area * np.cos(theta), ry / area * np.sin(theta)])
-
-# x, y, s, c = np.random.rand(4, 30)
-# s *= 10**2.
-
-# fig, ax = plt.subplots()
-# ax.scatter(x, y, s, c, marker=verts)
-
-# plt.show()
-
 
 if __name__ == "__main__":
     run(json.load(open("left-cube-left-side.json")))
