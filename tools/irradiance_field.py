1dc694fe2e886a9ed04fb83749574c7061810e5c
Chris Hellmuth <chellmuth@gmail.com>
2019-06-18 17:39:43 -0700

    Second plot shows incoming sample direction

--- tools/irradiance_field.py
+++ tools/irradiance_field.py
@@ -1,3 +1,4 @@
+import math
 import json
 
 # This import registers the 3D projection, but is otherwise unused.
@@ -6,8 +7,13 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import matplotlib.pyplot as plt
 import numpy as np
 
-def run(sample):
-    figure = plt.figure()
+def normalized(x, y, z):
+    length = math.sqrt(x**2 + y**2 + z**2)
+    assert length != 0
+
+    return (x / length, y / length, z / length)
+
+def location_plot(figure, sample):
     axes = figure.add_subplot(projection='3d')
 
     query_x, query_y, query_z = sample["QueryPoint"]
@@ -27,6 +33,38 @@ def run(sample):
     axes.set_xlabel("X")
     axes.set_ylabel("Z")
     axes.set_zlabel("Y")
+
+def direction_plot(figure, sample):
+    axes = figure.add_subplot(projection='3d')
+
+    query_x, query_y, query_z = sample["QueryPoint"]
+    axes.scatter(0, 0, 0, marker="^")
+
+    xs = []
+    ys = []
+    zs = []
+    for photon in sample["Results"]:
+        x, y, z = photon["source"]
+
+        wi = normalized(
+            x - query_x,
+            y - query_y,
+            z - query_z
+        )
+
+        xs.append(wi[0])
+        ys.append(wi[1])
+        zs.append(wi[2])
+
+    axes.scatter(xs, zs, ys)
+
+    axes.set_xlabel("X")
+    axes.set_ylabel("Z")
+    axes.set_zlabel("Y")
+
+def run(sample):
+    location_plot(plt.figure(1), sample)
+    direction_plot(plt.figure(2), sample)
 
     plt.show()
 
