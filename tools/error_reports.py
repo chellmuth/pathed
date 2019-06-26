1b9fccbeb39ce94104d75282dc8c2b3ad6050a81
Chris Hellmuth <chellmuth@gmail.com>
2019-06-26 14:24:14 -0700

    Add absolute error

--- tools/error_reports.py
+++ tools/error_reports.py
@@ -9,7 +9,10 @@ import pyexr
 def calculate_mse(test, gt):
     return np.sum((gt - test) ** 2)
 
-def build_dataset(gt, data_directory):
+def calculate_ae(test, gt):
+    return np.sum(np.abs(gt - test))
+
+def build_dataset(error_fn, gt, data_directory):
     mses = []
 
     i = 0
@@ -18,7 +21,7 @@ def build_dataset(gt, data_directory):
         if not exr_path.exists():
             break
 
-        mse = calculate_mse(
+        mse = error_fn(
             pyexr.read(str(exr_path)),
             gt
         )
@@ -28,9 +31,9 @@ def build_dataset(gt, data_directory):
 
     return mses
 
-def build_datasets(gt, plots):
+def build_datasets(error_fn, gt, plots):
     return [
-        build_dataset(gt, data_directory)
+        build_dataset(error_fn, gt, data_directory)
         for _, data_directory
         in plots
     ]
@@ -40,18 +43,25 @@ def power_of_two_formatter(x, pos):
     return int(2 ** x)
 
 def run(gt, plots):
-    ys_list = build_datasets(gt, plots)
+    errors = [ ("MSE", calculate_mse), ("AE", calculate_ae) ]
 
-    fig, ax = plt.subplots()
-    for i, ys in enumerate(ys_list):
-        xs = list(range(len(ys)))
-        ax.plot(xs, ys, label=plots[i][0])
+    fig, axs = plt.subplots(nrows=len(errors))
+    fig.suptitle("Convergence Plots")
 
-    ax.set(xlabel='spp', ylabel='mse', title='Convergence Plot')
-    ax.xaxis.set_major_formatter(power_of_two_formatter)
+    for error, ax in zip(errors, axs):
+        error_name, error_fn = error
 
-    plt.legend(loc='upper right')
+        ys_list = build_datasets(error_fn, gt, plots)
+        for i, ys in enumerate(ys_list):
+            xs = list(range(len(ys)))
+            ax.plot(xs, ys, label=plots[i][0])
 
+            ax.set(xlabel='spp (log)', ylabel=error_name)
+            ax.xaxis.set_major_formatter(power_of_two_formatter)
+
+            ax.legend(loc='upper right')
+
+    plt.tight_layout(rect=[0, 0, 1, 0.95]) # rect fixes suptitle clipping
     plt.show()
 
 if __name__ == "__main__":
