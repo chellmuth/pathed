8d3b1ef346554c5357011f064fd70dec7fb2de02
Chris Hellmuth <chellmuth@gmail.com>
2019-06-29 11:17:30 -0700

    Put combined in a reportable directory

--- tools/exr_combine.py
+++ tools/exr_combine.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import json
 
 import click
 import pyexr
@@ -26,10 +27,36 @@ def build_datasets(data_sources):
         in data_sources
     ]
 
+def find_scene(data_sources):
+    scenes = set()
+    for data_source in data_sources:
+        report = json.load(open(Path(data_source) / "report.json"))
+        scenes.add(report["scene"])
+
+    if len(scenes) > 1:
+        raise Exception(f"Different scenes present! {scenes}")
+
+    return scenes.pop()
+
+def write_report_json(out_path, scene):
+    content = {
+        "scene": scene,
+        "output_name": "Combined"
+    }
+
+    json.dump(content, open(out_path / "report.json", "w"))
+
 @click.command()
 @click.argument("data_sources", nargs=-1)
 def combine(data_sources):
     print(data_sources)
+
+    scene = find_scene(data_sources)
+
+    out_path = Path("./combined")
+    if not out_path.exists():
+        out_path.mkdir()
+
     datasets = build_datasets(data_sources)
 
     for i, spp_set in enumerate(zip(*datasets)):
@@ -37,9 +64,11 @@ def combine(data_sources):
         for bounce in spp_set[1:]:
             combined += bounce
 
-        exr_name = f"out-{2**i:04d}.exr"
-        print(f"Writing: {exr_name}")
-        pyexr.write(exr_name, combined)
+        exr_path = out_path / f"auto-{2**i:04d}.exr"
+        print(f"Writing: {exr_path}")
+        pyexr.write(str(exr_path), combined)
+
+    write_report_json(out_path, scene)
 
 if __name__ == "__main__":
     combine()
