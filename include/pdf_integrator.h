a7f644b2614bbf6ffbf06322dce1c7bf5488b828
Chris Hellmuth <chellmuth@gmail.com>
2019-09-11 15:44:12 -0700

    Shoot photons

--- include/pdf_integrator.h
+++ include/pdf_integrator.h
@@ -3,19 +3,26 @@
 #include "image.h"
 #include "integrator.h"
 #include "intersection.h"
-#include "scene.h"
+#include "kd_tree.h"
+#include "random_generator.h"
 #include "render_status.h"
+#include "scene.h"
 
+#include <memory>
 #include <vector>
 
 class PDFIntegrator : public Integrator {
 public:
+    PDFIntegrator();
+
     void run(
         Image &image,
         Scene &scene,
         std::function<void(RenderStatus)> callback,
         bool *quit
     ) override;
+
+    void preprocess(const Scene &scene, RandomGenerator &random) override;
 
 protected:
     void renderPDF(
@@ -24,4 +31,9 @@ protected:
         const Intersection &intersection
     );
 
+    void createPhotons(const Scene &scene, RandomGenerator &random);
+    Vector3 sample(const Vector3 &normal, RandomGenerator &random, float *pdf);
+
+    std::shared_ptr<DataSource> m_dataSource;
+    std::unique_ptr<KDTree> m_KDTree;
 };
--- src/pdf_integrator.cpp
+++ src/pdf_integrator.cpp
@@ -4,16 +4,86 @@
 #include "color.h"
 #include "globals.h"
 #include "job.h"
+#include "monte_carlo.h"
 #include "path_tracer.h"
-#include "random_generator.h"
 #include "ray.h"
 #include "sample.h"
 #include "transform.h"
 #include "util.h"
 #include "vector.h"
 
+#include <ctime>
 #include <iostream>
 #include <mutex>
+
+PDFIntegrator::PDFIntegrator()
+{
+    m_dataSource = std::make_shared<DataSource>();
+}
+
+Vector3 PDFIntegrator::sample(const Vector3 &normal, RandomGenerator &random, float *pdf) {
+    Vector3 hemisphereSample = UniformSampleHemisphere(random);
+    Transform hemisphereToWorld = normalToWorldSpace(normal);
+    *pdf = INV_TWO_PI;
+    return hemisphereToWorld.apply(hemisphereSample);
+}
+
+void PDFIntegrator::createPhotons(const Scene &scene, RandomGenerator &random)
+{
+    const int photonSamples = g_job->photonSamples();
+    const int photonBounces = g_job->photonBounces();
+
+    const int phiSteps = g_job->lightPhiSteps();
+    const int thetaSteps = g_job->lightThetaSteps();
+
+    for (int i = 0; i < photonSamples; i++) {
+        LightSample lightSample = scene.sampleLights(random);
+
+        float pdf;
+        Vector3 bounceDirection = sample(lightSample.normal, random, &pdf);
+
+        if (bounceDirection.dot(lightSample.normal) < 0.f) {
+            assert(false);
+            break;
+        }
+
+        Ray lightRay(lightSample.point, bounceDirection);
+
+        Color throughput = lightSample.light->getMaterial()->emit();
+        for (int bounce = 0; bounce < photonBounces; bounce++) {
+            Intersection intersection = scene.testIntersect(lightRay);
+            if (!intersection.hit) { break; }
+
+            throughput *= fmaxf(0.f, intersection.wi.dot(intersection.normal * -1.f));
+            if (throughput.isBlack()) { break; }
+
+            if (bounce > 0) { // don't guide towards direct lights
+                m_dataSource->points.push_back({
+                    intersection.point.x(),
+                    intersection.point.y(),
+                    intersection.point.z(),
+                    lightRay.origin(),
+                    throughput
+                });
+            }
+
+            Vector3 hemisphereSample = UniformSampleHemisphere(random);
+            Transform hemisphereToWorld = normalToWorldSpace(intersection.normal);
+            bounceDirection = hemisphereToWorld.apply(hemisphereSample);
+            lightRay = Ray(intersection.point, bounceDirection);
+
+            throughput *= intersection.material->f(intersection, bounceDirection);
+
+            // float invPDF = 1.f / INV_TWO_PI;
+            // throughput *= invPDF;
+        }
+    }
+}
+
+void PDFIntegrator::preprocess(const Scene &scene, RandomGenerator &random)
+{
+    createPhotons(scene, random);
+}
 
 void PDFIntegrator::run(
     Image &image,
@@ -21,6 +91,17 @@ void PDFIntegrator::run(
     std::function<void(RenderStatus)> callback,
     bool *quit
 ) {
+    RandomGenerator random;
+
+    {
+        printf("Beginning pre-process...\n");
+        std::clock_t begin = clock();
+        preprocess(scene, random);
+        std::clock_t end = clock();
+        double elapsedSeconds = double(end - begin) / CLOCKS_PER_SEC;
+        printf("Pre-process complete (%0.1fs elapsed)\n", elapsedSeconds);
+    }
+
     const int width = g_job->width();
     const int height = g_job->height();
 
@@ -29,7 +110,7 @@ void PDFIntegrator::run(
         radianceLookup[i] = 0.f;
     }
 
-    Ray ray = scene.getCamera()->generateRay(500, 500);
+    Ray ray = scene.getCamera()->generateRay(200, 200);
     Intersection intersection = scene.testIntersect(ray);
 
     const int spp = 1024;
