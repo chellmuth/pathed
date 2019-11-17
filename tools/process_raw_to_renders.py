efe18f1a57306231011245fec5af64367e0a4802
Chris Hellmuth <chellmuth@gmail.com>
2019-11-17 12:42:03 -0800

    Convert exrs

--- tools/process_raw_to_renders.py
+++ tools/process_raw_to_renders.py
@@ -2,12 +2,21 @@ import re
 from collections import namedtuple
 from pathlib import Path
 
+import numpy as np
+import pyexr
+
 import photon_reader
 
 Parts = namedtuple("Parts", ["identifier", "block"])
 
+def convert_rgb_to_single_channel(exr_path):
+    exr = pyexr.read(str(exr_path))
+
+    pixel_average = np.mean(exr, axis=2)
+    return np.stack([pixel_average, pixel_average, pixel_average], axis=2)
+
 def build_pdf_path(root_path, parts):
-    return root_path / f"pdf_{parts.identifier}-block_{parts.block}.bin"
+    return root_path / f"pdf_{parts.identifier}-block_{parts.block}.exr"
 
 def build_photon_path(root_path, parts):
     return root_path / f"photons_{parts.identifier}-block_{parts.block}.bin"
@@ -43,19 +52,19 @@ def valid_parts(raw_path):
 def run(raw_path, renders_path):
     renders_path.mkdir(exist_ok=True, parents=True)
 
-    dat_root = renders_path / "iteration-0000"
-    dat_root.mkdir(exist_ok=True)
+    iteration_root = renders_path / "iteration-0000"
+    iteration_root.mkdir(exist_ok=True)
 
     parts_to_index = {}
 
     for i, parts in enumerate(valid_parts(raw_path)):
         pdf_path = build_pdf_path(raw_path, parts)
-        # do stuff
+        pdf_exr = convert_rgb_to_single_channel(pdf_path)
+        pyexr.write(str(iteration_root / f"pdf_{i:05d}.exr"), pdf_exr)
 
         photon_path = build_photon_path(raw_path, parts)
-
         grid = photon_reader.build_grid(photon_path, (10, 10))
-        grid.export_dat(dat_root / f"photon-bundle_{i:05d}.dat")
+        grid.export_dat(iteration_root / f"photon-bundle_{i:05d}.dat")
 
 if __name__ == "__main__":
     run(
