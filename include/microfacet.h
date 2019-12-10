947391a140ac0b0170cf040492dfe3bcb5240a6b
Chris Hellmuth <chellmuth@gmail.com>
2019-12-09 21:29:50 -0800

    Beckmann lambda

--- include/microfacet.h
+++ include/microfacet.h
@@ -5,7 +5,7 @@
 #include <cmath>
 
 
-float beckmanD(const float alpha, const Vector3 &wh)
+float beckmannD(const float alpha, const Vector3 &wh)
 {
     const float tan2Theta = TangentFrame::tan2Theta(wh);
     if (std::isinf(tan2Theta)) { return 0.f; }
@@ -23,4 +23,22 @@ float beckmanD(const float alpha, const Vector3 &wh)
     const float denominator = M_PI * alpha2 * cos2Theta;
 
     return numerator / denominator;
+}
+
+float beckmannLambda(float alphaX, float alphaY, const Vector3 &w)
+{
+    const float absTanTheta = std::abs(TangentFrame::tanTheta(w));
+    if (std::isinf(absTanTheta)) { return 0.f; }
+
+    const float alpha = sqrtf(
+        TangentFrame::cos2Phi(w) * alphaX * alphaX
+        + TangentFrame::sin2Phi(w) * alphaY * alphaY
+    );
+
+    const float a = 1.f / (alpha * absTanTheta);
+
+    if (a >= 1.6f) { return 0.f; }
+
+    return (1 - 1.259f * a + 0.396f * a * a)
+        / (3.535f * a + 2.181f * a * a);
 }
