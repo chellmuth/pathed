1133e07c5ed4106847a1afb91d6a0ec8a21451f0
Chris Hellmuth <chellmuth@gmail.com>
2020-01-07 13:14:27 -0800

    Simplify

--- tools/grid_demo.py
+++ tools/grid_demo.py
@@ -20,15 +20,15 @@ def calc_next_time(rate, next_distance):
     return next_distance / rate
 
 def cell(current_point):
-    return (math.floor(current_point.x), math.floor(current_point.y))
+    return Point(math.floor(current_point.x), math.floor(current_point.y))
 
 def last_cell(exit_point):
     guess = cell(exit_point)
     if exit_point.x == int(exit_point.x):
-        guess = (guess[0] - 1, guess[1])
+        guess.x -= 1
 
     if exit_point.y == int(exit_point.y):
-        guess = (guess[0], guess[1] - 1)
+        guess.y -= 1
 
     return guess
 
@@ -55,22 +55,12 @@ def init():
 
     print("rates:", rates)
 
-    current_positions = Point(
-        entry_point.x,
-        entry_point.y
-    )
-
-    next_distances = Point(
-        calc_next_distance(current_positions.x),
-        calc_next_distance(current_positions.y)
-    )
-
     next_times = Point(
-        calc_next_time(rates.x, next_distances.x),
-        calc_next_time(rates.y, next_distances.y),
+        calc_next_time(rates.x, calc_next_distance(entry_point.x)),
+        calc_next_time(rates.y, calc_next_distance(entry_point.y)),
     )
 
-    current_cell = cell(current_positions)
+    current_cell = cell(entry_point)
     print("last cell:", last_cell(exit_point))
 
     print("visited cell:", current_cell)
@@ -79,10 +69,10 @@ def init():
     while current_cell != last_cell(exit_point):
         if next_times.x < next_times.y:
             next_times.x += 1. / rates.x
-            current_cell = (current_cell[0] + 1, current_cell[1])
+            current_cell.x += 1
         else:
             next_times.y += 1. / rates.y
-            current_cell = (current_cell[0], current_cell[1] + 1)
+            current_cell.y += 1
 
         print("visited cell:", current_cell)
         print(next_times)
