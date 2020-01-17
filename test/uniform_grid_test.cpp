da33beed09ca993c64703b4763c586f3cce5a1b2
Chris Hellmuth <chellmuth@gmail.com>
2020-01-16 22:30:50 -0800

    Interpolation

--- test/uniform_grid_test.cpp
+++ test/uniform_grid_test.cpp
@@ -25,3 +25,17 @@ TEST_CASE("grid bounds", "[uniform]") {
     REQUIRE(grid.interpolate(9.01f, 9.01f, 9.01f) == Approx(0.f));
     REQUIRE(grid.interpolate(9.f, 9.f, 9.f) == Approx(1.3f));
 }
+
+TEST_CASE("interpolation", "[uniform]") {
+    std::vector<float> gridData(10 * 10 * 10);
+
+    for (int i = 0; i < 10 * 10 * 10; i++) {
+        gridData[i] = i;
+    }
+
+    UniformGrid grid(10, 10, 10, gridData);
+
+    REQUIRE(grid.interpolate(1.f, 2.f, 3.f) == 321.f);
+    // (see derivation in test_interpolation.py)
+    REQUIRE(grid.interpolate(3.5f, 2.1f, 5.75f) == 599.5);
+}
