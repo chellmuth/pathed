8bb00795b69afca077a641c010003e8a33230f16
Chris Hellmuth <chellmuth@gmail.com>
2020-01-16 17:17:49 -0800

    Move regular tracker to its own file

--- include/grid_medium.h
+++ include/grid_medium.h
@@ -11,38 +11,6 @@
 #include <ostream>
 #include <vector>
 
-struct RegularTrackerStepResult {
-    bool isValidStep;
-
-    GridCell cell;
-    float cellTime;
-    float enterTime;
-    float currentTime;
-};
-
-class RegularTrackerState {
-public:
-    RegularTrackerState(const GridInfo &gridInfo, const Point3& entryPoint, const Point3 &exitPoint);
-
-    RegularTrackerStepResult step();
-
-private:
-    float worldTime(float gridTime);
-
-    const GridInfo m_gridInfo;
-    const Point3 m_entryPoint;
-    const Point3 m_exitPoint;
-
-    Vector3 m_rates;
-
-    float m_currentTime;
-    GridCell m_currentCell;
-
-    float m_endTime;
-
-    Vector3 m_nextTimes;
-};
-
 
 class GridMedium : public Medium {
 public:
