6e10f3b8f87b7477ce16dc540ded425f94e2aa0d
Chris Hellmuth <chellmuth@gmail.com>
2020-03-02 16:49:11 -0800

    Quick helper script

---
+++ tools/filter_nans.py
@@ -0,0 +1,51 @@
+import click
+import numpy as np
+import pyexr
+
+def neighborhood_average(exr, index):
+    neighbors = [
+        (
+            index[0] + y,
+            index[1] + x,
+            index[2]
+        )
+        for y in range(-1, 2)
+        for x in range(-1, 2)
+        if not (y == 0 and x == 0)
+    ]
+
+    height, width, _ = exr.shape
+    valid_neighbors = [
+        neighbor
+        for neighbor
+        in neighbors
+        if 0 <= neighbor[0] < height
+        and 0 <= neighbor[1] < width
+    ]
+
+    return sum(
+        exr[tuple(neighbor)]
+        for neighbor
+        in valid_neighbors
+    ) / len(valid_neighbors)
+
+@click.command()
+@click.argument("input_filename")
+@click.argument("output_filename")
+def run(input_filename, output_filename):
+    exr = pyexr.read(input_filename)
+
+    height, width, channels = exr.shape
+
+    nan_indices = np.argwhere(np.isnan(exr))
+
+    for index in nan_indices:
+        exr[tuple(index)] = neighborhood_average(exr, index)
+
+    pyexr.write(output_filename, exr)
+
+    print(f"Removed {len(nan_indices)} nans")
+
+
+if __name__ == "__main__":
+    run()
