1089bb6d8332c97b33ba08c089d7abbe90a83c94
Chris Hellmuth <chellmuth@gmail.com>
2020-03-13 09:52:47 -0700

    Copy function definitions from paper

--- src/ggx.cpp
+++ src/ggx.cpp
@@ -22,10 +22,27 @@ float GGX::pdf(const Vector3 &wh) const
 
 float GGX::D(const Vector3 &wh) const
 {
-    return 0.f;
+    const float alpha2 = m_alpha * m_alpha;
+
+    const float cos2Theta = TangentFrame::cos2Theta(wh);
+    const float cos4Theta = cos2Theta * cos2Theta;
+
+    const float tan2Theta = TangentFrame::tan2Theta(wh);
+    if (std::isinf(tan2Theta)) { return 0.f; }
+
+    const float sum = alpha2 + tan2Theta;
+    const float denominator = M_PI * cos4Theta * sum * sum;
+
+    return alpha2 / denominator;
 }
 
 float GGX::G(const Vector3 &wo, const Vector3 &wi) const
 {
-    return 0.f;
+    const float tan2Theta = TangentFrame::tan2Theta(wo);
+    if (std::isinf(tan2Theta)) { return 0.f; }
+
+    const float alpha2 = m_alpha * m_alpha;
+    const float sqrtTerm = (1 + alpha2 * tan2Theta);
+
+    return 2.f / (1 + std::sqrt(sqrtTerm));
 }
