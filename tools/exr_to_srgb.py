356832200b50d08813fe088314cb24ce4af68eac
Chris Hellmuth <chellmuth@gmail.com>
2020-05-01 14:03:13 -0700

    exr->srgb improvements

--- tools/exr_to_srgb.py
+++ tools/exr_to_srgb.py
@@ -25,17 +25,44 @@ def to_srgb(linear_image):
 def clip(image):
     return np.clip(image, 0.0, 1.0)
 
-def convert(exr_path, png_path):
-    image = pyexr.read(str(exr_path))
+def convert(image, png_path):
 
     srgb = clip(to_srgb(image))
     save_png(srgb, png_path)
 
+def grow(image, factor):
+    return image.repeat(factor, axis=0).repeat(factor, axis=1)
+
+def add_points(image, points):
+    colored = np.copy(image)
+
+    assert len(points) <= 3
+
+    colors = [ (1, 0, 0), (0, 1, 0), (0, 0, 1) ]
+
+    for point, color in zip(points, colors):
+        colored[point[1], point[0]] = color
+
+    return colored
+
 @click.command()
 @click.argument("exr_path", type=click.Path(exists=True))
 @click.argument("png_path", type=click.Path(exists=False, file_okay=True))
-def init(exr_path, png_path):
-    convert(exr_path, png_path)
+@click.option("--upsample", type=click.Choice(["1", "2", "4", "8"]), default="1")
+@click.option("--point", type=int, nargs=2, multiple=True)
+def init(exr_path, png_path, upsample, point):
+    upsample = int(upsample)
+    points = point
+
+    image = pyexr.read(str(exr_path))
+
+    if point:
+        image = add_points(image, points)
+
+    if upsample > 1:
+        image = grow(image, upsample)
+
+    convert(image, png_path)
 
 if __name__ == "__main__":
     init()
